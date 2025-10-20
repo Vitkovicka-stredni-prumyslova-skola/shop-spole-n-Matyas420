09edc878858691f2a10a7c5b78b3e3850012a908
Matyas420 <98987@student.vitkovickastredni.cz>
2025-10-20 10:25:08 +0200

    CV-složení base.html

--- app.py
+++ app.py
@@ -1,11 +1,42 @@
 from flask import Flask, render_template
 
-app = Flask(__name__, static_url_path='',static_folder='static')
+app = Flask(__name__)
 
 @app.route('/')
-def base():
+def index():
     return render_template('index.html')
 
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
+@app.route('/pricing')
+def pricing():
+    return render_template('pricing.html')
+
+@app.route('/faq')
+def faq():
+    return render_template('faq.html')
+
+@app.route('/blog_home')
+def blog_home():
+    return render_template('blog_home.html')
+
+@app.route('/blog_post')
+def blog_post():
+    return render_template('blog_post.html')
+
+@app.route('/portfolio_overview')
+def portfolio_overview():
+    return render_template('portfolio_overview.html')
+
+@app.route('/portfolio_item')
+def portfolio_item():
+    return render_template('portfolio_item.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
